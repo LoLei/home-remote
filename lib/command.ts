f037a2aad628294fbfe09bd2b96a81f76bd89be0
Lorenz Leitner <lrnz.ltnr@gmail.com>
2022-03-15 19:47:28 +0100

    Implement dynamic commands

--- lib/command.ts
+++ lib/command.ts
@@ -1,11 +1,14 @@
+import invariant from 'tiny-invariant';
 import util from 'util';
 
 const exec = util.promisify(require('child_process').exec);
 
-export async function execCommand(commandToExecute: string): Promise<ExecResult> {
-  if (!Object.values(COMMANDS).includes(commandToExecute)) {
-    throw new Error(`Command ${commandToExecute} is not a valid command.`);
-  }
+export async function execCommand(commandToExecute: ExecCommand): Promise<ExecResult> {
+  const execCommands = Object.values(ExecCommand);
+  invariant(
+    execCommands.includes(commandToExecute),
+    `Command ${commandToExecute} is not a valid command`,
+  );
   const { stdout, stderr } = await exec(commandToExecute);
   return {
     commandOutput: {
@@ -16,15 +19,30 @@ export async function execCommand(commandToExecute: string): Promise<ExecResult>
   };
 }
 
-export const COMMANDS = {
-  LS: 'ls', // For debugging
-  PLAYERCTL_PLAY: 'playerctl play',
-  PLAYERCTL_PAUSE: 'playerctl pause',
-  PLAYERCTL_TOGGLE: 'playerctl play-pause',
-  // Future: next, position, toggle...
-  PACTL_VOL_INC: 'pactl set-sink-volume 2 +5%',
-  PACTL_VOL_DEV: 'pactl set-sink-volume 2 -5%',
-};
+export enum ExecCommand {
+  PLAYERCTL_PLAY = 'playerctl play',
+  PLAYERCTL_PAUSE = 'playerctl pause',
+  PLAYERCTL_TOGGLE = 'playerctl play-pause',
+  // Future: next, position, ...
+  PACTL_VOL_INC = 'pactl set-sink-volume 2 +5%',
+  PACTL_VOL_DEC = 'pactl set-sink-volume 2 -5%',
+}
+
+export enum RequestCommand {
+  PLAYER_PLAY = 'play',
+  PLAYER_PAUSE = 'pause',
+  PLAYER_TOGGLE = 'toggle',
+  SYSTEM_VOL_INC = 'vol-inc',
+  SYSTEM_VOL_DEC = 'vol-dec',
+}
+
+export const commandMap: Map<RequestCommand, ExecCommand> = new Map([
+  [RequestCommand.PLAYER_PLAY, ExecCommand.PLAYERCTL_PLAY],
+  [RequestCommand.PLAYER_PAUSE, ExecCommand.PLAYERCTL_PAUSE],
+  [RequestCommand.PLAYER_TOGGLE, ExecCommand.PLAYERCTL_TOGGLE],
+  [RequestCommand.SYSTEM_VOL_INC, ExecCommand.PACTL_VOL_INC],
+  [RequestCommand.SYSTEM_VOL_DEC, ExecCommand.PACTL_VOL_DEC],
+]);
 
 type CommandOutput = {
   stdout?: string;
--- pages/api/control.ts
+++ pages/api/control.ts
@@ -1,16 +1,48 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { COMMANDS, execCommand } from '../../lib/command';
+import { commandMap, ExecCommand, execCommand, RequestCommand } from '../../lib/command';
 import type { ExecResult } from '../../lib/command';
+import invariant from 'tiny-invariant';
+
+type RequestBody = {
+  action: RequestCommand;
+  target: RequestTarget;
+};
+
+enum RequestTarget {
+  PLAYER = 'player',
+  SYSTEM = 'system',
+}
+
+const isValidRequestBody = (requestBody: Partial<RequestBody>): boolean => {
+  // This validation is annoying. This should happen automatically.
+  // Time to rewrite in another framework.
+  if (requestBody.action == null || requestBody.target == null) {
+    return false;
+  }
+
+  const requestCommands = Object.values(RequestCommand);
+  if (!requestCommands.includes(requestBody.action)) {
+    return false;
+  }
+
+  const requestTargets = Object.values(RequestTarget);
+  if (!requestTargets.includes(requestBody.target)) {
+    return false;
+  }
+
+  return true;
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ExecResult | string>,
 ) {
-  try {
-    const execResult = await execCommand(COMMANDS.PLAYERCTL_TOGGLE);
-    res.status(200).json(execResult);
-  } catch (e: unknown) {
-    console.error(e);
-    res.status(500).send((e as Error).toString());
-  }
+  const params = req.query as RequestBody;
+  invariant(isValidRequestBody(params), 'Request body is not valid.');
+
+  const commandToExecute = commandMap.get(params.action);
+  invariant(commandToExecute);
+
+  const execResult = await execCommand(commandToExecute);
+  res.status(200).json(execResult);
 }

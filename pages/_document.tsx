10f7e24dd648040f7378abcb449fd39a9b1ac14a
Lorenz Leitner <lrnz.ltnr@gmail.com>
2022-03-18 18:07:37 +0100

    Fix zoom on mobile in input element

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>{CssBaseline.flush()}</Head>
+        <Head>
+          {CssBaseline.flush()}
+          {/* Fix auto zoom in on mobile for fields with font size <16px https://stackoverflow.com/a/46254706/4644044 */}
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        </Head>
         <body>
           <Main />
           <NextScript />
